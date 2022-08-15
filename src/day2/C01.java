45da25228570a72e11b373b3e6f59d50f4bfd60e
filiz <flzylmz499@gmail.com>
2022-08-15 21:02:46 +0300

    v1

---
+++ src/day2/C01.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class C01 {
+    /*
+    1-git init --> Local repo olusturmak icin yani git ile klasorumuzun
+    icindeki dosyalri iliskilendirmwek icin kullanilir
+
+    2-git add --> working space den yani yerel dosyalarimi staging area ya gonderir
+     */
+}
